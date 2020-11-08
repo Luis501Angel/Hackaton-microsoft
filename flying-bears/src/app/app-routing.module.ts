95dbf2c5a098dd78df71a59d0a4a30bd28f25fdb
Luis501Angel <luis9925.la@gmail.com>
2020-11-07 19:00:39 -0600

    avance realizado

--- flying-bears/src/app/app-routing.module.ts
+++ flying-bears/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InicioComponent} from './components/inicio/inicio.component';
+import {CursoComponent} from './components/curso/curso.component';
+import {ActividadComponent} from './components/actividad/actividad.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent
+  },
+  {
+    path: 'curso',
+    component: CursoComponent
+  },
+  {
+    path: 'actividad',
+    component: ActividadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- flying-bears/src/app/app.module.ts
+++ flying-bears/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CursoComponent } from './components/curso/curso.component';
+import { ActividadComponent } from './components/actividad/actividad.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    NavbarComponent,
+    CursoComponent,
+    ActividadComponent
   ],
   imports: [
     BrowserModule,
